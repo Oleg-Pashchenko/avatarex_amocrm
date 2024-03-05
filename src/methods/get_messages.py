7ef440839cb65ad513916a43c5086b5192d2eeb4
olehandro <odpash.itmo@gmail.com>
2024-03-05 16:24:14 +0300

    docker

--- src/methods/get_messages.py
+++ src/methods/get_messages.py
@@ -43,7 +43,7 @@ async def get_unanswered_messages(host: str, headers: dict, pipeline_id: int, st
                 )
 
                 url = f"{amojo_host}messages/{amo_hash}/merge?stand=v16&offset=0&limit=20&chat_id%5B%5D={chat_id}&get_tags=true&lang=ru"
-                r = await session.get(url, , headers={"X-Auth-Token": chat_token})
+                r = await session.get(url,  headers={"X-Auth-Token": chat_token})
 
                 try:
                     messages_history = await r.json()
