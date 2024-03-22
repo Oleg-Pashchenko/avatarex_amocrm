add6b9fa7ecfe388c14704c52ca41ae39640213a
olehandro <odpash.itmo@gmail.com>
2024-03-22 18:36:42 +0300

    docker

---
+++ src/methods/add_tokens.py
@@ -0,0 +1,92 @@
+import asyncio
+import time
+
+import aiohttp
+from aiohttp import web
+
+from pydantic import BaseModel
+
+
+class AuthError(Exception):
+    pass
+
+
+class AddTokensData(BaseModel):
+    amo_host: str
+    access_token: str
+    refresh_token: str
+
+
+async def add_tokens_handler(request):
+    """
+    Создает access_token, refresh_token, amojo_id, chat_token каждые 10 минут
+    """
+    start_time = time.time()
+    try:
+        data = AddTokensData(**await request.json())
+        acc_token, refr_token, headers = await create_tokens(data.amo_host, data.amo_email, data.amo_password)
+
+        return web.json_response({
+            'status': True, 'answer': {
+                'headers': headers,
+                'access_token': acc_token,
+                'refresh_token': refr_token,
+                'amojo_id': await create_amojo_id(data.amo_host, headers),
+                'chat_token': await create_chat_token(data.amo_host, headers)
+            },
+            'execution_time': round(time.time() - start_time, 2)
+        })
+    except AuthError as e:
+        return web.json_response(
+            {'status': False, 'answer': f'{e}', 'execution_time': round(time.time() - start_time, 2)},
+            status=403)
+
+    except Exception as e:
+        return web.json_response(
+            {'status': False, 'answer': f'{e}', 'execution_time': round(time.time() - start_time, 2)},
+            status=400)
+
+
+async def create_amojo_id(host, headers):
+    url = f"{host}api/v4/account?with=amojo_id"
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(url, headers=headers,
+                                     )
+        data = await response.json()
+        return data["amojo_id"]
+
+
+async def create_chat_token(host, headers):
+    url = f"{host}ajax/v1/chats/session"
+    payload = {"request[chats][session][action]": "create"}
+    async with aiohttp.ClientSession() as session:
+        response = await session.post(url=url, headers=headers, data=payload,
+
+                                      )
+        data = await response.json()
+        return data["response"]["chats"]["session"]["access_token"]
+
+
+async def create_tokens(host, access_token, refresh_token):
+    cookies, csrf_token, headers = await _create_session(host)
+    headers['access_token'] = access_token
+    headers['refresh_token'] = refresh_token
+    headers['HOST'] = host.replace('https://', '').replace('/', '')
+    return access_token, refresh_token, headers
+
+
+async def _create_session(host):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(host,
+
+                               ) as response:
+            cookies = response.cookies
+            csrf_token = cookies.get("csrf_token").value
+            headers = {
+                "Accept": "application/json",
+                "X-Requested-With": "XMLHttpRequest",
+                "Cookie": f'session_id={cookies.get("session_id").value}; csrf_token={csrf_token};',
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+            }
+        return cookies, csrf_token, headers
+
--- src/router.py
+++ src/router.py
@@ -1,10 +1,11 @@
 from aiohttp import web
 
 from src.methods import *
-
+from src.methods.add_tokens import add_tokens_handler
 
 app = web.Application()
 app.router.add_post("/create-tokens/", create_tokens_handler)
+app.router.add_post("/add-tokens/", add_tokens_handler)
 app.router.add_post("/move-deal/", move_deal_handler)
 app.router.add_post("/get-fields/", get_fields_handler)
 app.router.add_post('/fill-field/', fill_fields_handler)
