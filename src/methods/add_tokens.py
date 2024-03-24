6f4754b6ba2f46c92b63e96ed97caf5b602b4496
olehandro <odpash.itmo@gmail.com>
2024-03-24 22:03:45 +0300

    docker

--- src/methods/add_tokens.py
+++ src/methods/add_tokens.py
@@ -2,6 +2,7 @@ import asyncio
 import time
 
 import aiohttp
+import requests
 from aiohttp import web
 
 from pydantic import BaseModel
@@ -13,7 +14,8 @@ class AuthError(Exception):
 
 class AddTokensData(BaseModel):
     amo_host: str
-    access_token: str
+    client_id: str
+    client_secret: str
     refresh_token: str
 
 
@@ -24,12 +26,13 @@ async def add_tokens_handler(request):
     start_time = time.time()
     try:
         data = AddTokensData(**await request.json())
+        access_token, refresh_token = _create_tokens(data)
         headers = {"Authorization": f'Bearer {data.access_token}'}
         return web.json_response({
             'status': True, 'answer': {
                 'headers': headers,
-                'access_token': data.access_token,
-                'refresh_token': data.refresh_token,
+                'access_token': access_token,
+                'refresh_token': refresh_token,
                 'amojo_id': await create_amojo_id(data.amo_host, headers),
                 'chat_token': await create_chat_token(data.amo_host, headers)
             },
@@ -92,3 +95,22 @@ async def _create_session(host):
                 "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
             }
         return cookies, csrf_token, headers
+
+
+def _create_tokens(data: AddTokensData):
+    print(data)
+    response = requests.post(f'{data.amo_host}oauth2/access_token', data={
+        'client_id': data.client_id,
+        'client_secret': data.client_secret,
+        'refresh_token': data.refresh_token,
+        'grant_type': 'refresh_token'
+    })
+    return response.json()['access_token'], response.json()['refresh_token']
+
+
+access_token, refresh_token = _create_tokens(AddTokensData(
+    client_id='4d7d3597-1dc1-4d11-91ef-3ea04b2bae33',
+    client_secret='v6DPpRMednQad8zOsOsRw2jVIZdShRps5vmylAllc36BVwb82oqYr88POzZUVw7e',
+    refresh_token='def502003c0c1d01755dc834fcf55d78c25070c071a06a908c4324ea9fdc15d1fbbd3c4dce0a84eda2655024cb3f3cae496877cb907d2e878da8b53ce9a43da8c3b3ab1f552f35f785af39a765d570f2ef791f506a75344a481c1005190d3481062d492e58d3ce6cc65870094024933c8ae5eb630d326bc003913412ed28871705a750eb38d14c1e9a0a556965dee9eddd90be1e8f1e78c5d19ade91ae4e21c1347a96d276f017003fe425174965ae635a6be02e5a101c529e45195a944aa21faffe92755a2b38b67c5ad0f904e0f8e035bfd0dcd321cc6604406e8beb7be052a94723a50d5d83c011157792c951bab9238c45b3054319678be1de1f4af33c5dc83950a98023044b48cae1df752fdd6dab325e597deb8badc40d5b9d2fdbad1ebe3b0ea5e59408a450fa2fb9268ffec8c8ff212df13d97b9a61aea844c4a63d4b960f352ae1194b829e05fae205a8b5179257219e656f64ab0b5b591229cfc96e3672b67c6bf8ff639335da2400954a9441ff4934b3eab42ee40df117a7ff84764445e915d18785d522e443d60d9334f80a55c5d886712c38c00cc34b9e0ce778c419cfc8c44760d35eef0c6081f70fc5908ce9574997128afac96d5896429755c67f4d9554923e88eae8c97ee2843496c976c29cad795',
+    amo_host='https://olegback2003.amocrm.ru/'
+))
--- src/methods/get_messages.py
+++ src/methods/get_messages.py
@@ -15,6 +15,17 @@ async def get_unanswered_messages(host: str, headers: dict, pipeline_id: int, st
             return []
 
         url = f"{host}ajax/v4/inbox/list"
+
+        if 'Authorization' in headers.keys():
+            headers = {
+                'Accept': '/',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Connection': 'keep-alive',
+                'User-Agent': 'PostmanRuntime/7.31.0',
+                'Authorization': headers['Authorization'],
+                'Host': host.replace('https://', '').replace('/', '')
+            }
+        print(headers)
         params = {
             "limit": 100,
             "order[sort_by]": "last_message_at",
@@ -26,7 +37,9 @@ async def get_unanswered_messages(host: str, headers: dict, pipeline_id: int, st
 
         async with aiohttp.ClientSession() as session:
             talks = await session.get(url=url, headers=headers, params=params, )
+            print(talks.status)
             talks = await talks.json()
+            print(talks)
             response = []
             await asyncio.sleep(1)
 
@@ -96,3 +109,14 @@ async def get_messages_handler(request):
             {'status': False, 'answer': f"{e}", 'execution_time': round(time.time() - start_time, 2),
              }, status=400
         )
+
+
+headers = {'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjFiNDdlZDRlOTU5ZjYwNDRiOTBmYWUxYWMxZmFiZjUyYjliNWJmMTNlZWEzZmI3MjAwZmRlOTU1MTAwYWE2YjJmZGRlZmYxNjg2ZjdiZGIwIn0.eyJhdWQiOiI4MDM2MDg5My1kNDViLTRiMzYtOTYyMi02MWU2OWQ0N2FlMjQiLCJqdGkiOiIxYjQ3ZWQ0ZTk1OWY2MDQ0YjkwZmFlMWFjMWZhYmY1MmI5YjViZjEzZWVhM2ZiNzIwMGZkZTk1NTEwMGFhNmIyZmRkZWZmMTY4NmY3YmRiMCIsImlhdCI6MTcxMTEyNTYzMCwibmJmIjoxNzExMTI1NjMwLCJleHAiOjE3MTEyMTIwMzAsInN1YiI6IjEwODQxMzIyIiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMxNjUyMjY2LCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJwdXNoX25vdGlmaWNhdGlvbnMiLCJjcm0iLCJub3RpZmljYXRpb25zIl0sImhhc2hfdXVpZCI6ImQ0NmUzZjg2LTliMzUtNDA1Ni04M2FlLWIxMjkwZWVlOWIzYyJ9.WhAflPz9xUX5j8HvhrPt_qXDa9cvcneLkKuNBdIDA-dPGy4mSVM6iUoSEPbTXeibW9xssSZQf78NiIbufhJwgzzlxIU2EzUMlhoDXQxZj9UKTIs8vpPxxVCfAAwvu8HfbYRqQUafLXIlgZD5iyk7ulftMUNSyxzkz-Q1cy0ENtIl8R4FM3BS7O2MzzFFYRBt-9X89_xhe1ObPSDKdbBlHXh31UDnl5XU5y33WHEgVv3yTce1oFWsVUQglJrOnt2K8UoheayRzk6-krZejZT4BGzsdydJVczted-kji8-FXGRUtSHWMohSUGggcWcotKEpQD55gv-58ATmqUFUmu0Cg', 'X-Requested-With': 'XMLHttpRequest'}
+print(asyncio.run(get_unanswered_messages(
+    host='https://olegpashchenkosyncer.amocrm.ru/',
+    headers=headers,
+    pipeline_id=7962174,
+    stage_ids=[65366830],
+    amo_hash='f1197894-9b50-452f-bc64-adbdb4f8221e',
+    chat_token='20975f25-1b8b-48d3-af08-b08a48324c65'
+)))
